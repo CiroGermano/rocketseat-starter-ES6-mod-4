58138c7c6749fb33e4ff8f898936cea169912e11
Ciro Moraes Germano <ciromrs@gmail.com>
2020-08-29 16:44:09 -0300

    Parte 3 - Render em tela

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formEl = document.querySelector('#repo-form');
+        this.listEl = document.querySelector('#repo-list');
 
         this.registerHandlers();
     }
@@ -20,8 +21,39 @@ class App {
             avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
             html_url: 'http://github.com/Rocketseat',
         });
+        
+        this.render();
+    }
 
-        console.log(this.repositories);
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('acessar'));
+
+            let listItemEl = document.createElement('li');
+
+
+
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
