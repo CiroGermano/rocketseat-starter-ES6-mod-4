d4ffecfad50fb2810646d1602fb6504d3edee910
Ciro Moraes Germano <ciromrs@gmail.com>
2020-08-29 16:00:36 -0300

    Parte 2 - Adicionar repositórios

--- src/main.js
+++ src/main.js
@@ -1,57 +1,28 @@
-import axios from 'axios';
+class App {
+    constructor() {
+        this.repositories = [];
 
+        this.formEl = document.querySelector('#repo-form');
 
-// 1
-const delay = () => new Promise(responseolve => setTimeout(responseolve, 1000));
+        this.registerHandlers();
+    }
 
-async function umPorSegundo() {
-    await delay();
-    console.log('1s');
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
 
-    await delay();
-    console.log('2s');
+    addRepository(event) {
+        event.preventDefault(); // não deixa o form recarregar a página, previnindo seu comportamento padrão
 
-    await delay();
-    console.log('3s');
-}
-umPorSegundo();
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida a sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/Rocketseat',
+        });
 
-
-// 2
-async function getUserFromGithub(user) {
-    try {
-        const responseponse = await axios.get(`https://api.github.com/users/${user}`);
-        console.log(responseponse);
-    } catch (error) {
-        console.log('Catch: Erro na API.');
+        console.log(this.repositories);
     }
 }
-getUserFromGithub('diego3g');
-getUserFromGithub('diego3g124123');
 
-
-// 3
-class Github {
-    static async getRepositories(repo) {
-        try {
-            const response = await axios.get(`https://api.github.com/repos/${repo}`);
-            console.log(response);
-        } catch (error) {
-            console.log("getRepositories catch: Falha na API.");
-        }
-    }
-}
-Github.getRepositories('Rocketseat/gatsby-themes');
-Github.getRepositories('rocketseat/dslkvmskv');
-
-
-// 4
-const buscaUsuario = async usuario => {
-    try {
-        const response = await axios.get(`https://api.github.com/users/${usuario}`);
-        console.log(response);
-    } catch (error) {
-        console.log("O usuário não existe.");
-    }
-}
-buscaUsuario('diego3g');
+new App();
